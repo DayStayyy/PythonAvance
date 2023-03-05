3699c251c9331763e23eba3315eaa509840e3e54
DayStayyy <daystayyy@gmail.com>
2023-03-05 19:44:32 +0100

    test CI/CD

---
+++ tests/test_addition.py
@@ -0,0 +1,7 @@
+import pytest
+from PythonAvance.basicFunction import addition
+
+@pytest.mark.parametrize("a, b, expected", [(1, 1, 2), (2, 5, 7), (812, 458421, 459233)])
+def test_addition(a, b, expected):
+    assert addition(a, b) == expected
+
