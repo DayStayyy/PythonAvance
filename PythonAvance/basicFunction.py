8dc1a45376e76917d0841ec77c1ecf0a901e4145
Adrien Clamadieu <adrien.clamadieu@ynov.com>
2023-03-06 16:11:29 +0100

    CI/CD
    * run on windows and mac os
    
    * code coverage
    
    * run on windows and mac os
    
    * run on windows and mac os
    
    * Python code coverage
    
    * Python code coverage
    
    * Python code coverage
    
    * commit workflows
    
    * pull request
    
    * seconde function
    
    * push and pull request blake
    
    * blake
    
    * blake
    
    * blake
    
    * push
    
    * commit blake
    
    * Pull request
    
    * Rm commit

--- PythonAvance/basicFunction.py
+++ PythonAvance/basicFunction.py
@@ -1,2 +1,8 @@
 def addition(a,b):
     return a+b
+
+def conditional(a,b):
+    if a>b:
+        return a
+    else:
+        return b
--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,7 +1,10 @@
 import pytest
-from PythonAvance.basicFunction import addition
+from PythonAvance.basicFunction import addition, conditional
 
 @pytest.mark.parametrize("a, b, expected", [(1, 1, 2), (2, 5, 7), (812, 458421, 459233)])
 def test_addition(a, b, expected):
     assert addition(a, b) == expected
 
+
+def test_conditional():
+    assert conditional(1, 2) == 2
