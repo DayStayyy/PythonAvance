ce6db732f3ebb10bededeb39703b0c041c1d8ff4
unknown <adrien.clamadieu@ynov.com>
2023-03-13 10:18:07 +0100

    base for test

--- PythonAvance/basicFunction.py
+++ PythonAvance/basicFunction.py
@@ -1,8 +1,2 @@
 def addition(a,b):
     return a+b
-
-def conditional(a,b):
-    if a>b:
-        return a
-    else:
-        return b
--- tests/test_addition.py
+++ tests/test_addition.py
@@ -4,7 +4,3 @@ from PythonAvance.basicFunction import addition, conditional
 @pytest.mark.parametrize("a, b, expected", [(1, 1, 2), (2, 5, 7), (812, 458421, 459233)])
 def test_addition(a, b, expected):
     assert addition(a, b) == expected
-
-
-def test_conditional():
-    assert conditional(1, 2) == 2
