89e594b733bd1d96653fa30b8b1b57f18fbcabfd
Clementadm <clement_demoussac@hotmail.fr>
2023-03-13 10:08:58 +0100

    push for test + png/anne_chaudes

--- PythonAvance/main.py
+++ PythonAvance/main.py
@@ -30,7 +30,6 @@ async def page(request: Request):
         "page": "info",
         "soustitre": "les infos"
     }
-    # return templates.TemplateResponse("base.html", {"request": request, "data": data})
     return templates.TemplateResponse("info.html", {"request": request, "data": data})
 
 @app.get("/page/png", response_class=HTMLResponse)
@@ -39,5 +38,13 @@ async def page(request: Request):
         "page": "png",
         "soustitre": "les png"
     }
-    # return templates.TemplateResponse("base.html", {"request": request, "data": data})
     return templates.TemplateResponse("png.html", {"request": request, "data": data})
+
+
+@app.get("/page/png/annee_chaudes", response_class=HTMLResponse)
+async def page(request: Request):
+    data = {
+        "page": "Quelles sont les 10 années les plus chaudes depuis 1996 ?",
+        "soustitre": "Explications :"
+    }
+    return templates.TemplateResponse("annee_chaudes.html", {"request": request, "data": data})
